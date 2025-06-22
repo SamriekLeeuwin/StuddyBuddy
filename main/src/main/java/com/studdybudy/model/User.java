cc48f10781409862f643495c1eae9a7c2fa4d3fd
SamriekLeeuwin <semriekk@gmail.com>
2025-06-23 00:25:38 +0200

    changes de columnames for better readability for postgre

--- main/src/main/java/com/studdybudy/model/User.java
+++ main/src/main/java/com/studdybudy/model/User.java
@@ -3,34 +3,35 @@ package com.studdybudy.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name= "User")
-@Setter
+@Table(name = "app_user")
 @Getter
+@Setter
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long id;
+    private Long id;
 
-    @Column(name="User_Name" , nullable = false)
+    @NotBlank(message = "First name is required")
+    @Column(name = "user_first_name", nullable = false)
     private String firstname;
 
-    @Column(name= "User_LastName", nullable = false)
+    @NotBlank(message = "Last name is required")
+    @Column(name = "user_last_Name", nullable = false)
     private String lastname;
 
-   @Email
-   @NotBlank
-    @Column(name= "User_Email", nullable = false, unique = true)
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    @Column(name = "user_email", nullable = false, unique = true)
     private String email;
 
-
-    @Column(name ="User_Password" ,nullable = false)
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
+    @Column(name = "user_password", nullable = false)
     private String password;
-
-
-
-
 }
