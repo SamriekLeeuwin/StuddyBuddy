8d33f8dcb8a276971e73f9b3aff6f3b8e44e4fd4
SamriekLeeuwin <semriekk@gmail.com>
2025-06-23 22:02:08 +0200

    changed controller for better readabilitz

--- main/src/main/java/com/studdybudy/controller/AuthController.java
+++ main/src/main/java/com/studdybudy/controller/AuthController.java
@@ -5,14 +5,9 @@ import com.studdybudy.dto.UserRequestDTO;
 import com.studdybudy.service.AuthService;
 import com.studdybudy.service.UserService;
 import jakarta.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,33 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/auth")
 public class AuthController {
 
-
-     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
-     private final UserService userService;
      private final AuthService authService;
 
-@Autowired
-     public  AuthController(UserService userService, AuthService authService){
-          this.userService= userService;
+     public AuthController(AuthService authService) {
           this.authService = authService;
      }
 
+     @PostMapping("/signup")
+     public ResponseEntity<AuthResponseDTO> register(@Valid @RequestBody UserRequestDTO request) {
+          AuthResponseDTO response = authService.createUser(request);
+          return new ResponseEntity<>(response, HttpStatus.CREATED);
+     }
 
      @PostMapping("/login")
      public ResponseEntity<AuthResponseDTO> login(@Valid @RequestBody UserRequestDTO request) {
-          logger.info("Login attempt for user: {}", request.username());
           AuthResponseDTO response = authService.login(request);
-          logger.info("Login successful for user: {}", request.username());
           return ResponseEntity.ok(response);
      }
-
-     // eindpoint for t
-     @PostMapping("/signup")
-     public ResponseEntity<AuthResponseDTO> register(@Valid @RequestBody UserRequestDTO request) {
-          logger.info("Registering user: {}", request.username());
-          AuthResponseDTO response = authService.createUser(request);
-          logger.info("User registered: {}", request.username());
-          return new ResponseEntity<>(response, HttpStatus.CREATED);
-     }
 }
-
