82ed3dcd08494158842580b4defc23c74be838d9
SamriekLeeuwin <semriekk@gmail.com>
2025-06-24 14:53:22 +0200

    changed the param for the user object

--- main/src/main/java/com/studdybudy/service/JwtService.java
+++ main/src/main/java/com/studdybudy/service/JwtService.java
@@ -1,5 +1,6 @@
 package com.studdybudy.service;
 
+import com.studdybudy.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
@@ -7,25 +8,25 @@ import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class JwtService {
 
     private final String issuer;
-
     private final Duration ttl;
-
     private final JwtEncoder jwtEncoder;
 
-    public String generateToken(final String username) {
-        final var claimsSet = JwtClaimsSet.builder()
-                .subject(username)
+    public String generateToken(final User user) {
+        JwtClaimsSet claimsSet = JwtClaimsSet.builder()
+                .subject(user.getUsername())
                 .issuer(issuer)
+                .claim("roles", List.of("ROLE_" + user.getRole().name()))
+                .issuedAt(Instant.now())
                 .expiresAt(Instant.now().plus(ttl))
                 .build();
 
         return jwtEncoder.encode(JwtEncoderParameters.from(claimsSet))
                 .getTokenValue();
     }
-
 }
