8c573cd49eb900f1a9f12162005e633705b03307
SamriekLeeuwin <semriekk@gmail.com>
2025-06-24 14:24:12 +0200

    added a service for  create token,.

--- main/src/main/java/com/studdybudy/service/AuthService.java
+++ main/src/main/java/com/studdybudy/service/AuthService.java
@@ -1,5 +1,11 @@
 package com.studdybudy.service;
 
+import com.studdybudy.dto.AuthResponseDTO;
+import com.studdybudy.dto.UserRequestDTO;
+import com.studdybudy.model.Role;
+import com.studdybudy.model.User;
+import com.studdybudy.service.JwtService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.studdybudy.controller.AuthController;
 import com.studdybudy.repository.UserRepository;
@@ -7,4 +13,47 @@ import com.studdybudy.repository.UserRepository;
 @Service
 public class AuthService {
 
+    private final PasswordEncoder passwordEncoder;
+    private final JWTUtil jwtUtil;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+
+    public AuthService(PasswordEncoder passwordEncoder, JWTUtil jwtUtil, UserRepository userRepository, JwtService jwtService) {
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+    }
+
+    public AuthResponseDTO createUser(UserRequestDTO request) {
+        if (userRepository.findByUsername(request.username()).isPresent()) {
+            throw new IllegalArgumentException("Username already exists!");
+        }
+
+        String hashedPassword = passwordEncoder.encode(request.password());
+
+        User newUser = new User(
+                request.username(),
+                hashedPassword,
+                Role.USER
+        );
+
+        userRepository.save(newUser);
+
+        String token = jwtService.generateToken(newUser);
+
+        return new AuthResponseDTO(token, newUser.getUsername());
+    }
+
+    public AuthResponseDTO login(UserRequestDTO request) {
+        User user = userRepository.findByUsername(request.username())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid username or password"));
+
+        if (!passwordEncoder.matches(request.password(), user.getPassword())) {
+            throw new IllegalArgumentException("Invalid username or password");
+        }
+
+        String token = jwtUtil.generateToken(user);
+        return new AuthResponseDTO(token, user.getUsername());
+    }
 }
