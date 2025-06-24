859d8cbfbab7f2b890aab3647f0a891867a6cd22
SamriekLeeuwin <semriekk@gmail.com>
2025-06-24 14:53:51 +0200

    removed the method findbyusername

--- main/src/main/java/com/studdybudy/repository/UserRepository.java
+++ main/src/main/java/com/studdybudy/repository/UserRepository.java
@@ -1,8 +1,14 @@
 package com.studdybudy.repository;
 
+import com.studdybudy.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    Optional<User> findByUsername(String username);
 }
