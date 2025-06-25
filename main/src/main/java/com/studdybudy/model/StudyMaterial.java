961424edda4dd197c8979b12ba26b434f77d6c55
SamriekLeeuwin <semriekk@gmail.com>
2025-06-25 23:56:27 +0200

    added study material entity

---
+++ main/src/main/java/com/studdybudy/model/StudyMaterial.java
@@ -0,0 +1,40 @@
+package com.studdybudy.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@ToString(exclude = "user") // voorkom eindeloze loop in toString
+public class StudyMaterial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+    @Size(max = 100)
+    @Column(nullable = false)
+    private String title;
+
+    @NotBlank(message = "Description is required")
+    @Size(max = 1000)
+    @Column(nullable = false, length = 1000)
+    private String description;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Column(nullable = false)
+    private LocalDate uploadDate = LocalDate.now();
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
