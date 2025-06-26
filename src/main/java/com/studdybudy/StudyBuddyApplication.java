2a9b6690ef531de306ed1fbf691b3912bd45a65a
SamriekLeeuwin <semriekk@gmail.com>
2025-06-26 21:35:59 +0200

    CHANGED README

---
+++ src/main/java/com/studdybudy/StudyBuddyApplication.java
@@ -0,0 +1,11 @@
+package com.studdybudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBuddyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudyBuddyApplication.class, args);
+    }
+}
